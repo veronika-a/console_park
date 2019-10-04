4c21271a71b605d82f10ada71c0580f83ef9fb48
veronika-a <andrianika00@gmail.com>
2019-10-04 20:57:36 +0300

    +textinconsole

--- Program.cs
+++ Program.cs
@@ -8,27 +8,45 @@ namespace ConsolePark
         static void Main(string[] args)
         {
 
+            console();
+
+            
+
+        }
+        static void console() {
+            Console.WriteLine("Ввести номер машины");
+            string car = Console.ReadLine();
+            Console.WriteLine("Ввести номер места парковки");
+            int place = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            park(car, place);
+        }
+
+
+        static void park( string c,int p) {
             Car car = new Car();
-            car.Name = "666";
+            car.Name = c;
 
             ParkPlace parkPlace = new ParkPlace();
             parkPlace.IsEmpty = true;
-            parkPlace.Number = 13;
-            
+            parkPlace.Number = p;
+
 
             Parking parking = new Parking();
             parking.Price = 100;
             parking.ParkPlaces.Add(parkPlace);
 
             ParkingProcess parkingProcess = new ParkingProcess();
-            ParkingResult parkingResult= new ParkingResult();
-            parkingResult =(ParkingResult)parkingProcess.ParkCar(parking, car, 3);
+            ParkingResult parkingResult = new ParkingResult();
+            parkingResult = (ParkingResult)parkingProcess.ParkCar(parking, car, 3);
             Console.WriteLine("ParkingNumber: " + parkingResult.ParkingNumber);
             Console.WriteLine("Car: " + parkingResult.Car.Name);
             Console.WriteLine("Price: " + parkingResult.Price);
             Console.WriteLine("Start: " + parkingResult.Start);
             Console.WriteLine("End: " + parkingResult.End);
-
+            Console.WriteLine();
+            console();
         }
         
         
